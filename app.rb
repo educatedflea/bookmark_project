fc2fd94955a04dbcffcac02e29e1641bf4c7e9e6
Ziyao Zheng <ziyaozheng@Ziyaos-MacBook-Pro.local>
2020-03-09 16:40:39 +0000

    g #psql set up successfully

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ class BookmarkManager < Sinatra::Base
 	get '/' do 
 		"Bookmark Manager"
 	end 
-
+	#1 User story: see the list of the book mark
 	get '/bookmarks' do 
 		@bookmarks = Bookmark.all
 		erb :'bookmarks/index'
