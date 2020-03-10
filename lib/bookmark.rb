f40d6159a761dcc3de92ea8c3968a34de6ee83a9
Ziyao Zheng <ziyaozheng@Ziyaos-MacBook-Pro.local>
2020-03-10 12:35:21 +0000

    #attached database to ruby

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,9 +1,12 @@
+require 'pg' 
+
 class Bookmark
 	def self.all 
-		[
-			"http://www.makersacademy.com",
-      "http://www.destroyallsoftware.com",
-      "http://www.google.com"
-		]
+		#connect to pg database
+		connection = PG.connect(dbname:'bookmark_manager')
+		# run querys on connected database, assign query returns to a variable 
+		result = connection.exec("SELECT * FROM bookmarks;")
+		# map the returns to the variable i.e, allow the return values to update the variable 
+		result.map {|bookmark| bookmark['url']}
 	end 
 end 
